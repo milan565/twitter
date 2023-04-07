7fb3f844cbfcc2631da7f68aaef6eb82b8ac62a1
milan aryal <milanaryal74@gmail.com>
2023-04-07 21:27:20 +0545

    first commit

--- src/ppage/Errorpage.js
+++ src/ppage/Errorpage.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Rightcom from '../component/Rightcom'
 
 export default function Errorpage() {
   return (
     <>
-     <h1>this is error page</h1> 
+      <div className='bbody_container'>
+        <div className='bbody_inner_container'>
+            <div className='main_container'> 
+            <div className='left_main_container'>
+              <div className='error_left_box'>
+                <div className='error_in_box'>
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFLSitzsIWi-EZxOmYVxwJr6wPOL1vui1SLw&usqp=CAU'/>
+                    <h1>404</h1>
+                    <p>Page not found</p>
+                </div>
+              </div>
+            </div>
+            <div className='right_main_container'>
+              <div className='error_right_box'>
+                <Rightcom/>
+              </div>
+            </div>
+            </div>
+         
+        </div>
+        
+      </div>
     </>
   )
 }
